b44b325b690c2b9da3cedef9f512d35454d4eada
billygotclout <williamsoluwademilade@gmail.com>
2022-05-17 15:01:56 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import WelcomeScreen from "./app/Screens/WelcomeScreen";
+
+export default function App() {
+  return (<ViewImageScreen/>);
+  
+}
